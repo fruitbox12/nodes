bfca215207948c4c2f74178de5795cf290b45ee2
fruitbox12 <dylanwong007@gmail.com>
2024-06-02 01:31:34 -0700

    Update zapier.js

--- nodes/Zapier/zapier.js
+++ nodes/Zapier/zapier.js
@@ -35,7 +35,7 @@ class Zapier {
             {
                 label: 'Authorization',
                 name: 'credentialMethod',
-                type: 'options',
+                type: 'string',
                 options: [
                     {
                         label: 'Bearer Token Auth',
